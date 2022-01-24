a82d84d12eabc37d4ed0819b55334d8a33bffae5
VerboonMatthijs8amo2 <1042849@mborijnland.nl>
2022-01-24 16:11:22 +0100

    over ons gemaakt
    (meeste gedaan door noah ik heb gewoon wat kleine aanpassingen gemaakt_

---
+++ app/src/main/java/com/example/amazigh_app/Over_ons.java
@@ -0,0 +1,14 @@
+package com.example.amazigh_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Over_ons extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_over_ons);
+    }
+}
