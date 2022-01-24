ac0c911164fecd27923744731dcd9783f28f2e48
VerboonMatthijs8amo2 <1042849@mborijnland.nl>
2022-01-24 15:51:12 +0100

    over ons activity gemaakt en op juiste plek gezet

--- app/src/main/java/com/example/amazigh_app/OverOnsActivity.java
+++ app/src/main/java/com/example/amazigh_app/OverOnsActivity.java
@@ -1,14 +1,13 @@
 package com.example.amazigh_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+public class OverOnsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.overons_page);
     }
 }
